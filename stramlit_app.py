4d4bce1467b129cd8a41422b95b675db25299b9a
Rodrigo Lacerda Arraes de Alencar <50839107+rodrigolaa@users.noreply.github.com>
2024-08-13 11:08:27 +0200

    loc function

--- stramlit_app.py
+++ stramlit_app.py
@@ -21,9 +21,11 @@ session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 # st.stop()
+
+#Convert the snowpark dataframe to a pandas dataframe so we can use the loc function
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+# st.dataframe(pd_df)
+# st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
@@ -36,6 +38,10 @@ if ingredients_list:
     
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        
         fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{fruit_chosen}")
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width = True)
 
