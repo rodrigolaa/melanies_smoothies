879dbaed00719d55f05550e96a2705fd9c86a3a8
Rodrigo Lacerda Arraes de Alencar <50839107+rodrigolaa@users.noreply.github.com>
2024-08-13 10:48:27 +0200

    fix

--- stramlit_app.py
+++ stramlit_app.py
@@ -46,6 +46,6 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success(f'Your Smoothie is ordered! {name_on_order}', icon="✅")
-        
-fv_df = st.dataframe(data=fruityvice_response.json(), use_container_user = True)
+
+fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width = True)
 
