cbdcbe1869a0dd4c5214cef3d71172cd71af5631
Rodrigo Lacerda Arraes de Alencar <50839107+rodrigolaa@users.noreply.github.com>
2024-08-13 10:46:45 +0200

    new json print

--- stramlit_app.py
+++ stramlit_app.py
@@ -5,7 +5,7 @@ from snowflake.snowpark.functions import col
 #New Section to add API Data
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+st.text(fruityvice_response.json())
 
 # Write directly to the app
 st.title("My Parents New Healthy Diner")
